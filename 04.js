95b9004eb0f47d5c5607d735fce0f99ee5e61d6a
aliqaaa <syabilaaliqaluthfiana@gmail.com>
2025-10-02 12:22:08 +0700

    Update 04.js

--- 04.js
+++ 04.js
@@ -1,18 +1,31 @@
 class Mahasiswa {
   constructor(nama, nim, nilai) {
     this.nama = nama; // Atribut publik
-    this.nim = nim; // Atribut publik
-    let _nilai = nilai; // Atribut private menggunakan closure
+    this.kelas = kelas; // Atribut publik
+    this.prodi = prodi; // Atribut publik
+    let _nim = nim; // Atribut privat menggunakan closure
+    this.nilai = nilai; // Atribut publik
 
     // Getter untuk mengakses nilai
-    this.getNilai = function() {
-      return nilai;
+    this.getNim = function() {
+      return _nim;
     };
     // Setter untuk mengubah nilai dengan validasi
-    this.setNilai = function(nilaiBaru) {
-      if (nilaiBaru >= 0 && nilaiBaru <= 100) { 
-        _nilai = nilaiBaru;
-      }else { console.log('Nilai harus antara 0 dan 100'); }
+    this.setNim = function(nimBaru) {
+      if (/^\d{4}$/.test(nimBaru)) { 
+        _nim = nimBaru;
+      }else { console.log("Nim harus terdiri dari 4 digit angka"); }
     };
   }
 }
+let mhs1 = new Mahasiswa("Tiara", "ip241", "informatika", "0064", 85);
+let mhs2 = new Mahasiswa("widya", "mp241", "informasi", "0457", 70);
+
+console.log(mhs1.nama, "-', mhs1.kelas,"-", mhs1.prodi, mhs1.getnim(), "-", mhs1.nilai);
+console.log(mhs2.nama, "-', mhs2.kelas,"-", mhs2.prodi, mhs2.getnim(), "-", mhs2.nilai);
+            
+mhs1.setNim("0055");
+mhs2.setNim("0234");
+
+console.log(mhs1.nama, "-', mhs1.getnim(), "-", mhs1.nilai;
+console.log(mhs2.nama, "-', mhs2.getnim(), "-", mhs2.nilai;
