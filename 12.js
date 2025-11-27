6f7bfbe427880532c75d689ece14578e1a4b268d
aliqaaa <syabilaaliqaluthfiana@gmail.com>
2025-11-27 22:45:49 +0700

    Update 12.js

--- 12.js
+++ 12.js
@@ -13,3 +13,25 @@ console.log("Daftar Mahasiswa:");
 mahasiswaList.forEach(mhs =>
 console.log(`${mhs.nama} - ${mhs.jurusan}`)
 );
+
+// Form Pendaftaran ( Bisa Tambah Data Baru)
+
+// Ambil data lama
+let data = JSON.parse(localStorage.getItem("pendaftar")) || [];
+
+// Pendaftar baru
+const baru = { 
+    nama: "Aliqa", 
+    email: "aliqa@ipwija.ac.id", 
+    gender: "Perempuan",
+    jurusan: "Informatika"
+};
+
+// Push lalu simpan ulang
+data.push(baru);
+localStorage.setItem("pendaftar", JSON.stringify(data));
+
+// Tampilkan
+console.log("Pendaftaran Mahasiswa");
+console.log("Daftar Pendaftar:");
+data.forEach(p => console.log(`${p.nama} | ${p.email} | ${p.gender} | ${p.jurusan}`));
